89945a1c60794451aad03cdcda5006d29a45b01c
Ubuntu <ubuntu@ip-172-31-89-117.ec2.internal>
2019-11-21 05:01:09 +0000

    Exercise 5 Completed

--- world.js
+++ world.js
@@ -2,13 +2,26 @@
 
 window.onload = () => {
     let search = this.document.getElementById("lookup");
+    let search_cities = this.document.getElementById("lookup-cities");
 
     this.$("country").submit(function (e) {
         e.preventDefault();
     });
-
+    search_cities.addEventListener("click", ()=>{
+         this.$.ajax({
+            type: "GET",
+            url: "world.php",
+            data: this.$("#country").serialize()+"&context=cities",
+            success: function (data) {
+                let result = document.getElementById("result");
+                result.innerHTML = data;
+            },
+            dataType: "text"
+        });
+        
+        
+    } );
     search.addEventListener("click", () => {
-        console.log("Test");
         this.$.ajax({
             type: "GET",
             url: "world.php",
--- world.php
+++ world.php
@@ -1,6 +1,7 @@
 <?php
 
-$country = $_GET['country'];
+$country = filter_var(trim($_GET["country"]), FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+$context = filter_var(trim($_GET["context"]), FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 $host = getenv('IP');
 $username = 'lab7_user';
 $password = 'my_password';
@@ -9,10 +10,39 @@ $dbname = 'world';
 ?>
 
 <?php
-
-
-if (isset($country) || !empty($country)) 
-{
+ 
+if ($context === 'cities'){
+   $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT c.name, c.district, c.population FROM cities c JOIN countries cs ON
+c.country_code = cs.code WHERE cs.name = '$country'");
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    
+    
+    echo '<table>';
+       
+        echo '<tr>';
+            echo '<th> Name</th>';
+            echo '<th>District</th>';
+            echo '<th>Population</th>';
+        echo '</tr>';
+        
+        foreach ($results as $row){
+        echo '<tr>';
+            echo '<td>' . $row['name'] . '</td>';
+            echo '<td>' . $row['district'] . '</td>';
+            echo '<td>' . $row['population'] . '</td>';
+        
+        echo '</tr>';
+        }
+        
+    
+     
+    echo '</table>';
+    
+    
+    
+}else if (isset($country) || !empty($country)) {
+    
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
