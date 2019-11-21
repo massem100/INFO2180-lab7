e5a57565fef474e80cdaa5ad11efd0c608b26b16
Ubuntu <ubuntu@ip-172-31-89-117.ec2.internal>
2019-11-21 03:18:33 +0000

    Exercise 4 Completed

--- world.php
+++ world.php
@@ -16,10 +16,31 @@ if (isset($country) || !empty($country))
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
-    echo '<ul>';
-    foreach ($results as $row) 
-    {
-        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-    }
-    echo '</ul>';
+    
+    
+       echo '<table>';
+       
+        echo '<tr>';
+            echo '<th> Country Name</th>';
+            echo '<th>Continent</th>';
+            echo '<th>Independence Year</th>';
+            echo '<th>Head of State</th>';
+        echo '</tr>';
+        
+        foreach ($results as $row){
+        echo '<tr>';
+            echo '<td>' . $row['name'] . '</td>';
+            echo '<td>' . $row['continent'] . '</td>';
+            echo '<td>' . $row['independence_year'] . '</td>';
+            echo '<td>' . $row['head_of_state'] . '</td>';
+        
+        echo '</tr>';
+        }
+        
+    
+     
+    echo '</table>';
+
 }
+?>
+
